3752e96ad55ebe4c36cd4f9fa5ceeaa2248865b2
stefandoucette <44303343+stefandoucette@users.noreply.github.com>
2020-07-12 14:31:35 -0600

    Small additions to player and game_board.

---
+++ gameplay/game_board.py
@@ -0,0 +1,12 @@
+import player
+import game_position
+import card_deck
+
+class GameBoard:
+    def __init__(self):
+        self.game_positions = []
+        self.players = []
+        self.card_deck = []
+
+    def createGamePositions(self):
+        pass
---
+++ gameplay/player.py
@@ -0,0 +1,6 @@
+import mover
+
+class Player:
+    def __init__(self, name="PlayerName"):
+        self.name = name  # Player's name
+        self.mover = []   # new mover
